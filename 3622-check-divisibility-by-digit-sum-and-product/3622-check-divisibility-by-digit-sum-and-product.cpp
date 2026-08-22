49035ecfa51c65675b5906054a5860277a0c59d9
AshishRaj2212 <Rajashish2212@gmail.com>
2026-08-22 20:40:39 +0530

    Time: 0 ms (100%), Space: 7.8 MB (68.55%) - LeetHub

---
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int num = n;
+        int sum = 0;
+        int prod = 1;
+        while(n>0){
+            sum += n % 10;
+            prod *= n % 10;
+            n = n/10;
+        }
+        if(num % (sum + prod) == 0){
+            return true;
+        }
+        return false;        
+    }
+};
