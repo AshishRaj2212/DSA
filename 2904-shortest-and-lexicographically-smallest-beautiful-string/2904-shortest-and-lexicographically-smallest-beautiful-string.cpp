1ee537331705ab27f36a6189c2fc04e0e68dcdbd
AshishRaj2212 <Rajashish2212@gmail.com>
2026-08-26 21:01:54 +0530

    Time: 6 ms (13.85%), Space: 11.3 MB (5.79%) - LeetHub

---
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        int n = s.length();
+        
+        for(int len = k; len<=n; len++){
+            string result = "";
+            for(int start = 0; start <= n-len; start++){
+                string temp = s.substr(start, len);
+                
+                int countOfOnes = 0;
+                for(char &ch : temp){
+                    countOfOnes += (ch == '1') ? 1 : 0;
+                }
+                if(countOfOnes == k){
+                    if(result.empty() || temp < result){
+                        result = temp;
+                    }
+                }
+            }
+            if(!result.empty()){
+                return result;
+            }
+        }
+        return "";
+    }
+};
