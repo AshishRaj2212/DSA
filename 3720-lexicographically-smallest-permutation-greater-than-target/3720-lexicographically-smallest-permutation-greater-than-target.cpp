0251c4a087e9b82a72c7d9027a007f170e45bee6
AshishRaj2212 <Rajashish2212@gmail.com>
2026-08-27 21:42:07 +0530

    Time: 0 ms (100%), Space: 9.9 MB (82.17%) - LeetHub

---
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string result;
+
+    bool solve(string& curr, vector<int>& count, string& target, int idx, bool greater){
+        if(idx == target.length()){
+            if(greater == true){
+                result = curr;
+                return true;
+            }
+            return false;
+        }
+
+        for(char ch = 'a'; ch<='z'; ch++){
+            if(count[ch-'a'] == 0) continue;
+            if(greater == false && ch < target[idx]) continue;
+
+            curr.push_back(ch);
+            count[ch-'a']--;
+            bool isGreater = greater || ch > target[idx];
+            if(solve(curr, count, target, idx+1, isGreater)){
+                return true;
+            }
+
+            curr.pop_back();
+            count[ch-'a']++;
+        }
+        return false;
+    }
+    
+    string lexGreaterPermutation(string s, string target) {
+        vector<int> count(26, 0);
+        for(char &ch : s){
+            count[ch-'a']++;
+        }
+        string curr;
+
+        solve(curr, count, target, 0, false);
+
+        return result;
+    }
+};
