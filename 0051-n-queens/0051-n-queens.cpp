936ed43df1fd7fa6b75ad97a06d4c8385197d5ea
AshishRaj2212 <Rajashish2212@gmail.com>
2026-08-31 21:00:24 +0530

    Time: 11 ms (11.46%), Space: 12.3 MB (15.42%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -2,28 +2,9 @@ class Solution {
 public:
     vector<vector<string>> result;
     int N;
-
-    bool isValid(vector<string>& board, int row, int col){
-        //checking upward
-        for(int i = row - 1; i>=0; i--){
-            if(board[i][col] == 'Q'){
-                return false;
-            }
-        }
-        //checking diagonal
-        for(int i = row-1, j = col+1; i>=0 && j<N; i--, j++){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
-        }
-        //checking anti diagonal
-        for(int i = row-1, j = col-1; i>=0 && j>=0; i--, j--){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
-        }
-        return true;
-    }
+    unordered_set<int> cols;
+    unordered_set<int> diag;
+    unordered_set<int> antiDiag;
 
     void solve(vector<string>& board, int row){
         if(row >= N){
@@ -31,11 +12,24 @@ public:
             return;
         }
         for(int col = 0; col < N; col++){
-            if(isValid(board, row, col)){
-                board[row][col] = 'Q';
-                solve(board, row+1);
-                board[row][col] = '.';
+            int diagConst = row+col;
+            int antiDiagConst = row-col;
+
+            if(cols.find(col) != cols.end() || diag.find(diagConst) != diag.end() || antiDiag.find(antiDiagConst) != antiDiag.end()){
+                continue;
             }
+            cols.insert(col);
+            diag.insert(diagConst);
+            antiDiag.insert(antiDiagConst);
+
+            board[row][col] = 'Q';
+            
+            solve(board, row+1);
+            
+            cols.erase(col);
+            diag.erase(diagConst);
+            antiDiag.erase(antiDiagConst);
+            board[row][col] = '.';
         }
     }
     vector<vector<string>> solveNQueens(int n) {
